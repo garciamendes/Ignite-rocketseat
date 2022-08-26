7a6e9cde0cec392c34bb01b2b31c7481e229816e
Aron Matheus Garcia Mendes <mmendes@morphus.com.br>
2022-08-26 19:30:09 -0300

    base body

---
+++ project-01/src/components/Profile/index.tsx
@@ -0,0 +1,8 @@
+// Local
+import './profile.scss'
+
+export function Profile() {
+  return (
+    <h1>Profile</h1>
+  )
+}
--- project-01/src/containers/Home/index.tsx
+++ project-01/src/containers/Home/index.tsx
@@ -1,11 +1,18 @@
 // Project
 import { Header } from '../../components/Header'
+import { Profile } from '../../components/Profile'
+
+// Local
+import './home.scss'
 
 export function Home() {
   return (
     <>
       <Header />
-      <h1>Home</h1>
+      <div className='container_main'>
+        <Profile />
+        <h1>Home</h1>
+      </div>
     </>
   )
 }
